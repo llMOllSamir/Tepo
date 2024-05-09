8d52dff0dbbf6b91baa40af46177fdc821373729
Mohamed Samir <121590212+llMOllSamir@users.noreply.github.com>
2024-05-09 22:43:39 +0300

    fix responsive

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -16,7 +16,7 @@ export default async function Products({ params, searchParams }) {
   products = await products.json()
 
   return (
-    <section className=" grow my-10 container gap-10 mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
+    <section className=" grow my-10 container gap-10 mx-auto grid grid-cols-1 px-16  sm:px-2  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
       {products.data.map((product) => (
         <>
           <Card key={product._id} product={product} />
